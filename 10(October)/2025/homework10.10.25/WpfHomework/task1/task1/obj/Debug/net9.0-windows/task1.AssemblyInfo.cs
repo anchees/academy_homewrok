b651818fbffe582f2cf43060fa95b23a36b30d6f
Lenar <tyttrost1@gmail.com>
2026-06-16 03:26:03 +0500

    megamnogo

--- 10(October)/2025/homework10.10.25/WpfHomework/task1/task1/obj/Debug/net9.0-windows/task1.AssemblyInfo.cs
+++ 10(October)/2025/homework10.10.25/WpfHomework/task1/task1/obj/Debug/net9.0-windows/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24c38f15d7c3c1f33bac259c6c00257cf0088624")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4714b2c32656ad30bc84cbfb5f555eb81d5a5c62")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
